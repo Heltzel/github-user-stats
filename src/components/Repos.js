4fe7e07e2eb5cb41ea23e76ce6896b7e0d4bc970
Heltzel <heltzel.service@hotmail.com>
2020-12-15 15:01:32 +0100

    Refactor example chart part 2 : conv. to dynamic

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,28 +2,33 @@ import React from 'react'
 import styled from 'styled-components'
 import { GithubContext } from '../context/context'
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts'
-const chartData = [
-  {
-    label: 'HTML',
-    value: '13',
-  },
-  {
-    label: 'Css',
-    value: '23',
-  },
-  {
-    label: 'Javasript',
-    value: '80',
-  },
-]
 
 const Repos = () => {
   const { repos } = React.useContext(GithubContext)
-  console.log(repos)
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item
+    if (!language) {
+      return total
+    }
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 }
+    } else {
+      total[language] = { ...total[language], value: total[language].value + 1 }
+    }
+    return total
+  }, {})
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value
+    })
+    .slice(0, 5)
+
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <ExampleChart data={chartData} />
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={languages} />
       </Wrapper>
     </section>
   )
